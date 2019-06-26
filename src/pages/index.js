fcc637ddd04493daa4c2cc63ca55d77c0a8b994c
sandond77 <sandu@ucdavis.edu>
2019-06-25 22:11:43 -0700

    Finished pages tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
+import Link from "gatsby-link"
 
-export default () => <div>Hello world!</div>
+export default () => 
+	<div style={{color:'tomato'}} >
+		<h1> Hello World </h1>
+		<p> This is a paragraph! </p>
+		<Link to="/page-2/">Page 2 </Link>
+		<Link to="/dir1/page-3/">Page 3 </Link>
+	</div>
+
+
---
+++ src/pages/page-2.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Link from "gatsby-link"
+
+export default () => 
+	<div style={{color:'tomato'}} >
+		<h1> Page 2 </h1>
+		<Link to="/"> Go Home </Link>
+	</div>
+
+
