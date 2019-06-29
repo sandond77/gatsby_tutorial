06517cc7a2d7e6618fec12c52d8edbf7c5c4e2f7
sandond77 <sandu@ucdavis.edu>
2019-06-29 02:03:57 -0700

    created a 404.js page for deployment errors

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Link from "gatsby-link"
+
+
+export default () => 
+		<div style={{color:'tomato'}} >
+			<h1> 404 ERROR</h1>
+			<Link to="/"> Return to Main Page </Link>
+			<br />
+		</div>
+
+
